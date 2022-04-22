8cef54c1d6f97cba39b03b8fb18269566047bc6e
Fenosoa Randrianjatovo <98966847+FenosoaRandrianjatovo@users.noreply.github.com>
2022-04-22 03:33:50 +0000

    Add files via upload

---
+++ modules/preprocessing.py
@@ -0,0 +1,39 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
+from collections import Counter
+from num2words import num2words
+
+import nltk
+import os
+import string
+import numpy as np
+import copy
+import pandas as pd
+import pickle
+import re
+import math
+
+
+def convert_lower_case(data):
+    return np.char.lower(data)
+
+def remove_stop_words(data):
+    stop_words = stopwords.words('english')
+    words = word_tokenize(str(data))
+    new_text = ""
+    for w in words:
+        if w not in stop_words and len(w) > 1:
+            new_text = new_text + " " + w
+    return new_text
+
+def remove_punctuation(data):
+    symbols = "!\"#$%&()*+-./:;<=>?@[\]^_`{|}~\n"
+    for i in range(len(symbols)):
+        data = np.char.replace(data, symbols[i], ' ')
+        data = np.char.replace(data, "  ", " ")
+    data = np.char.replace(data, ',', '')
+    return data
+
+def remove_apostrophe(data):
+    return np.char.replace(data, "'", "")
