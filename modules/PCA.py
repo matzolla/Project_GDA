cf1d95f7d3ec08245d66972d1535f0a40859c066
Wedoo.ai <57256473+matzolla@users.noreply.github.com>
2024-04-18 23:59:09 +0100

    Update PCA.py

--- modules/PCA.py
+++ modules/PCA.py
@@ -7,7 +7,9 @@ def PCA(df, k):
   # step 1: calculate the Mean normalization of x
   normalized_x = x - np.mean(x,axis=0)
   # step 2: calculate covariance matrix from the normalized_x
+  
   cov = np.cov(normalized_x, rowvar=False)
+  
   # make the parameter rowvar = False; check the documentation of np.cov to see why..
   # step 3: compute the eigen values and eigen vectors
   eig_val, eig_vec = np.linalg.eig(cov)
